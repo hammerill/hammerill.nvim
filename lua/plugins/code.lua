45bb8561350a8aad351cf1730f96e2538cdbc90a
hammerill <kyrylo@hammerill.com>
2024-12-08 19:23:45 +0100

    Add VimBeGood game

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -5,6 +5,7 @@ return {
   "nvim-treesitter/nvim-treesitter",			-- Semantic syntax highlighting
   "tpope/vim-fugitive",					-- Git commands from editor
   "lewis6991/gitsigns.nvim",				-- Git signs on the left
+  "ThePrimeagen/vim-be-good",				-- Learn Vim game
   {
     "nvim-telescope/telescope.nvim",			-- File selector
     dependencies = { "nvim-lua/plenary.nvim" },
